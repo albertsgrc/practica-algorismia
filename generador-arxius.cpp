76c24534fbf92451915127afe3f57c20c826da84
Albert Segarra Roca <albertsgrc@gmail.com>
2015-12-23 13:59:14 +0100

    Refactored generator and added random seed

--- generador-arxius.cpp
+++ generador-arxius.cpp
@@ -6,44 +6,72 @@
 
 using namespace std;
 
-vector<int> dicc;
-
-void makeArxiu1(int n){
-    dicc = vector<int>();
-    ofstream diccionari;
-    diccionari.open ("arxiu1");
-    for (int i = 0; i<n;++i){
-        int value= rand();
-        dicc.push_back(value);
-        diccionari << value<<endl;
-    }
+void usage() {
+    cout << "Usage: generador-arxius n proporcio" << endl;
 }
 
-void makeArxiu2(int n,double percentatge){
-    int porpot= n*percentatge/100;
-    vector<int> text = vector<int> (2*n);
-    for (int i =0;i<2*n;++i) text[i] = rand();
-    for (int i = 0;i<porpot;++i){
-        int j = rand() % n; // j = position of a random value from arxiu1
-        int k = rand() % 2*n; //k= position in arxiu2 of the random value
-        int value = dicc[j];
-        text[k] = value;
-    }
-    ofstream file;
-    file.open("arxiu2");
-    for(int i=0;i<2*n;++i) file<< text[i]<<endl;
-    file.close();
+void error(const string& msg) {
+    cerr << msg << endl;
+    exit(1);
 }
 
-int main(){
+void crear_arxiu1(int n, vector<int>& diccionari) {
+    diccionari = vector<int>(n);
+
+    ofstream arxiu1;
+    arxiu1.open("arxiu1");
+
+    for (int& x : diccionari) {
+        x = rand();
+        arxiu1 << x << endl;
+    }
+
+    arxiu1.close();
+}
+
+void crear_arxiu2(int n, double proporcio, const vector<int>& diccionari) {
+    int repetits = n*proporcio/100;
+    vector<int> text(2*n);
+
+    for (int&x : text) x = rand();
+
+    for (int i = 0; i < repetits; ++i) {
+        int index_arxiu1 = rand()%n;
+        int index_arxiu2 = rand()%(2*n);
+        text[index_arxiu2] =  diccionari[index_arxiu1];
+    }
+
+    ofstream arxiu2;
+    arxiu2.open("arxiu2");
+    for (int x : text) arxiu2 << x << endl;
+
+    arxiu2.close();
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 3) usage();
+
     int n;
-    cout<< "Enter size of the dictionary: ";
-    cin>>n;
-    cout<<endl;
-    makeArxiu1(n);
-    double percentatge;
-    cout<< "Enter porpotion: ";
-    cin>>percentatge;
-    cout<<endl;
-    makeArxiu2(n,percentatge);
+    try { n = stoi(argv[1]); }
+    catch (const exception& ia) {
+        error("El primer argument ha de ser un natural");
+    }
+
+    double proporcio;
+    try { proporcio = stod(argv[2]); }
+    catch (const exception& ia) {
+        error("El segon argument ha de ser un real entre 0 i 1");
+    }
+
+    if (n < 0)
+        error("La n ha de ser positiva");
+
+    if (proporcio > 1.0 or proporcio < 0)
+        error("La proporcio ha d'estar entre 0 i 1");
+
+    srand(time(NULL));
+
+    vector<int> diccionari;
+    crear_arxiu1(n, diccionari);
+    crear_arxiu2(n, proporcio, diccionari);
 }
