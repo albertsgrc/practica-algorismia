4ad732c8b5f5cdecb80d66cc20a926191218d4d8
Outer2g <aol.treballs@gmail.com>
2015-12-25 17:43:52 +0100

    Bloom

---
+++ src/algoritmos/bloom-filter.cc
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include "io.hpp"
+#include "cronometro.hpp"
+using namespace std;
+
+class Bloom{
+private:
+    int bits;
+    vector<Bool> bit_vector;
+    int hash_functions;
+    int hash_it(int k, int key){
+        int aux = this->hash_functions;
+        int aux2 = this->bits;
+        return (k*aux+50+aux2)*200 % aux2;
+    }
+public:
+    Bloom(int n,int k){
+        bit_vector = vector<Bool> (n,false);
+        hash_functions = k;
+        bits = n;
+    }
+
+    void add(int a){
+        for (int i = 0;i<hash_functions;++i){
+            int value = hash_functions(i,a);
+            bit_vector[value] = true;
+        }
+    }
+
+    bool contains(int a){
+        for (int i =0;i<hash_functions;++i){
+            int value = hash_functions(i,a);
+            if (not bit_vector[value]) return false;
+        }
+        return true;
+    }
+};
+void algoritmo(const VI& diccionario, const VI& texto, VB& resultado) {
+// El algoritmo deberá calcular las estadísticas de ejecución sólo si se
+// ha compilado el programa en versión estadísticas. Así se evita
+// que el tiempo de ejecución se vea afectado por el cálculo extra.
+// Esto se puede hacer por ejemplo como vemos en el main
+// (#if _STATS_ ... #endif)
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 3) usage(argv[0]);
+
+    VI diccionario, texto;
+    lee_entrada(argv[1], argv[2], diccionario, texto);
+
+    vector<bool> resultado(texto.size());
+
+    Cronometro<> c;
+    c.iniciar();
+    algoritmo(diccionario, texto, resultado);
+    c.finalizar();
+
+    for (bool b : resultado) cout << b << endl;
+
+    #if _STATS_
+    ofstream estadisticas;
+    estadisticas.open(argc > 3 ? argv[3] : "estadisticas.json");
+    escribe_json(
+        {
+            {"tamaño_diccionario", diccionario.size()},
+            {"tamaño_texto", texto.size()},
+            // poner aquí todas las estadísticas, con su par nombre/valor
+            // ejemplo:
+            {
+                "total_comparaciones_busqueda_fracaso",
+                valor_de_total_comparaciones_busqueda_fracaso
+            }
+        }
+        ,
+        estadisticas);
+    #else
+    ofstream tiempo;
+    tiempo.open(argc > 3 ? argv[3] : "tiempo.out");
+    tiempo << c.transcurrido();
+    tiempo.close();
+    #endif
+}
